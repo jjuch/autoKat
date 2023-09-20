adf7ca5ad9a1d0cbfc687153c8f79a83473003ef
jjuchem <jasper.juchem@UGent.be>
2023-09-20 23:33:47 +0200

    add game logic

--- autoKat/animals.py
+++ autoKat/animals.py
@@ -20,23 +20,66 @@ class Sheep():
     def __init__(self, init_location):
         self.current_location = init_location
         self.distance_from_dog = (0.0, 0.0)
-        self.velocity_scaling = 10
+        self.velocity_scaling = 50
+        self.max_velocity = 5
 
-    def calculate_new_coordinate(self, dog_location):
+    def in_house_bool(self, sheep_house):
+        x_range = (sheep_house['xy'][0], sheep_house['xy'][0] + sheep_house['width'])
+        y_range = (sheep_house['xy'][1] , sheep_house['xy'][1] +  sheep_house['height'])
+    
+        if (x_range[0] < self.current_location[0] < x_range[1]) and (y_range[0] < self.current_location[1] < y_range[1]):
+            return True
+        else:
+            return False
+
+    def calculate_new_coordinate(self, dog_location, field_size):
+        def check_boundaries(val, idx):
+            if val > field_size[idx]:
+                val = field_size[idx]
+            elif val < 0:
+                val = 0
+            return val
+        
         distance_to_dog = self.current_location[0] - dog_location[0], self.current_location[1] - dog_location[1]
 
-        new_location = (
-            self.current_location[0] + self.velocity_scaling/distance_to_dog[0],
-            self.current_location[1] + self.velocity_scaling/distance_to_dog[1]
-        )
+        new_x = self.current_location[0] + np.sign(self.velocity_scaling/distance_to_dog[0]) * min(self.max_velocity, abs(self.velocity_scaling/distance_to_dog[0]))
+        new_x += (2*random.random() - 1) * self.velocity_scaling/50
+        new_x = check_boundaries(new_x, 0)
+
+        new_y = self.current_location[1] + np.sign(self.velocity_scaling/distance_to_dog[1]) * min(self.max_velocity, abs(self.velocity_scaling/distance_to_dog[1]))
+        new_y += (2*random.random() - 1) * self.velocity_scaling/50
+        new_y = check_boundaries(new_y, 1)
+
+        new_location = (new_x, new_y)
+    
+        print("sheep: ", new_location)
+        
         self.current_location = new_location
 
 
 class Dog():
     def __init__(self):
         self.current_location = (0.0, 0.0)
+        self.last_pointer_location = (0.0, 0.0)
+        self.approach_rate = 2
 
-    def update_dog_location(self, field_size: tuple):
-        x_coord = random.uniform(0, field_size[0])
-        y_coord = random.uniform(0, field_size[1])
-        self.current_location = (x_coord, y_coord)
+    def update_dog_location(self):
+        error = (
+            self.last_pointer_location[0] - self.current_location[0],
+            self.last_pointer_location[1] - self.current_location[1]
+        )
+        gain_error = self.approach_rate * error
+        self.current_location = (
+            gain_error[0] + self.current_location[0],
+            gain_error[1] + self.current_location[1]
+        )
+
+    def mouse_move(self, *args):
+        event = args[0]
+        x, y = event.xdata, event.ydata
+        print("Mouse: ", x, ", ", y)
+        if x is not None and y is not None:
+            self.last_pointer_location = (x, y)
+
+
+    
--- autoKat/game.py
+++ autoKat/game.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mppatch
 import time
 
-from autoKat.animals import Flock, Dog
+from autoKat.animals import Flock, Dog, Sheep
 
 
 class Game():
@@ -11,34 +12,64 @@ class Game():
         self.currentField = None
         self.flock = Flock(self.size)
         self.dog = Dog()
+        self.sheep_house = {
+            'xy': (10, 20),
+            'width': 5,
+            'height': 10
+        }
+        self.refresh_rate = 0.1 # seconds
+        self.add_sheep_time = 10 # seconds
         self.simulate()
 
     def hasWon(self):
         # Check if the game is over
-        return False
+        return len(self.flock.sheeps) == 0
 
 
     def simulate(self):
+        # create playfield
         plt.ion()
         fig, ax = plt.subplots()
+        plt.xlim([-10, self.size[0] + 10])
+        plt.ylim([-10, self.size[1] + 10])
+        plt.connect('motion_notify_event', self.dog.mouse_move)
+        sheep_house_patch = mppatch.Rectangle(self.sheep_house['xy'], self.sheep_house['width'], self.sheep_house['height'])
+        ax.add_patch(sheep_house_patch)
+
+
         point_dog, = ax.plot(*self.dog.current_location, marker='x')
         sheep_plots = []
         for sheep in self.flock.sheeps:
             sheep_data, = ax.plot(*sheep.current_location, marker='o')
             sheep_plots.append(sheep_data)
         
+        
+        tick = 0
+        max_ticks = int(self.add_sheep_time / self.refresh_rate)
 
-        while not self.hasWon():
-            self.dog.update_dog_location(self.size)
+        while not self.hasWon() and plt.fignum_exists(fig.number):
+            # update dog location
+            self.dog.update_dog_location()
             point_dog.set_data(*self.dog.current_location)
 
+            # update sheep's position
             for i, sheep in enumerate(self.flock.sheeps):
-                sheep.calculate_new_coordinate(self.dog.current_location)
-                print(sheep.current_location)
+                sheep.calculate_new_coordinate(self.dog.current_location, self.size)
                 sheep_plots[i].set_data(*sheep.current_location)
+                if sheep.in_house_bool(self.sheep_house):
+                    self.flock.sheeps.remove(sheep)
+                    sheep_plots.remove(sheep_plots[i])
+            if tick == max_ticks:
+                if len(self.flock.sheeps) < self.flock.max_flock_size:
+                    self.flock.sheeps.append(Sheep((0,0)))
+                    sheep_data, = ax.plot(*self.flock.sheeps[-1].current_location, marker='o')
+                    sheep_plots.append(sheep_data)
+                tick = 0
+            tick += 1
+            # print(tick, "/", max_ticks)
             fig.canvas.draw()
             fig.canvas.flush_events()
-            time.sleep(1)
+            time.sleep(self.refresh_rate)
             
             
 

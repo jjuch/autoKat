f3b21d5a7824926db9dc5a04995eb7fd4ecb3609
Michiel Van den Berghe <michiel.vdb@gmail.com>
2023-10-02 20:33:45 +0200

    add maelstrom

--- autokat/web/app.tsx
+++ autokat/web/app.tsx
@@ -2,10 +2,11 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { useGameState } from "./hooks/gamestate";
 import { useCallback, useEffect, useState } from "react";
-import { useAnimationFrame } from "./hooks/animation";
+import { useAnimationFrame, useAnimationTime } from "./hooks/animation";
+import { Maelstrom } from "./maelstrom";
 
-const SCREEN_WIDTH = 1024
-const SCREEN_HEIGHT = 768
+const SCREEN_WIDTH = 1024;
+const SCREEN_HEIGHT = 768;
 
 function Tail({
   x,
@@ -32,12 +33,9 @@ function Tail({
   );
 }
 
-function Dog({ x, y, heading = 0, color = "green" }) {
+function Sheep({ x, y, heading = 0, color = "green" }) {
   const transform = `translate(${x}px,${y}px) rotate(${heading}rad)`;
-  const [t, setT] = useState<number>(0);
-  useAnimationFrame(({ t: animationTime }) => {
-    setT(animationTime);
-  });
+  const t = useAnimationTime();
   return (
     <g
       className="entity"
@@ -53,6 +51,58 @@ function Dog({ x, y, heading = 0, color = "green" }) {
       <Tail x={0} y={0} t={t} color={color}></Tail>
       <Tail x={0} y={15} t={t} color={color}></Tail>
       <Tail x={0} y={30} t={t} color={color}></Tail>
+    </g>
+  );
+}
+
+function Finlet({x, length, color, t, direction = 'down'}) {
+  return (
+    <g>
+      <ellipse
+        cx={x}
+        cy={(direction === 'down' ? 1 : -1) * 34}
+        rx={8}
+        ry={length * 2}
+        stroke={color}
+        style={{ transform: `rotate3d(1, 0, 0, ${Math.sin(Math.PI * t) * Math.PI / 4}rad)`, strokeWidth: 1 }}
+      ></ellipse>
+    </g>
+  )
+}
+function Dog({ x, y, heading, color = "blue" }) {
+  const transform = `translate(${x}px,${y}px) rotate(${heading}rad)`;
+  const t = useAnimationTime();
+  return (
+    <g
+      className="entity"
+      style={{
+        transform,
+      }}
+    >
+      <Finlet x={-45} length={14} color={color} t={t - .3}></Finlet>
+      <Finlet x={-30} length={16} color={color} t={t - .2}></Finlet>
+      <Finlet x={-15} length={18} color={color} t={t - .1}></Finlet>
+      <Finlet x={0} length={20} color={color} t={t + .0}></Finlet>
+      <Finlet x={15} length={18} color={color} t={t + .1}></Finlet>
+      <Finlet x={30} length={16} color={color} t={t + .2}></Finlet>
+      <Finlet x={45} length={14} color={color} t={t + .3}></Finlet>
+      <Finlet x={-45} length={14} color={color} t={t - .3} direction="up"></Finlet>
+      <Finlet x={-30} length={16} color={color} t={t - .2} direction="up"></Finlet>
+      <Finlet x={-15} length={18} color={color} t={t - .1} direction="up"></Finlet>
+      <Finlet x={0} length={20} color={color} t={t + .0} direction="up"></Finlet>
+      <Finlet x={15} length={18} color={color} t={t + .1} direction="up"></Finlet>
+      <Finlet x={30} length={16} color={color} t={t + .2} direction="up"></Finlet>
+      <Finlet x={45} length={14} color={color} t={t + .3} direction="up"></Finlet>
+      <ellipse
+        cx={0}
+        cy={0}
+        rx={60}
+        ry={20}
+        stroke={color}
+        style={{ strokeWidth: 2 }}
+      ></ellipse>
+      <circle cx={60} cy={10} stroke={color} r={6}></circle>
+      <circle cx={60} cy={-10} stroke={color} r={6}></circle>
     </g>
   );
 }
@@ -101,8 +151,15 @@ function App() {
   }, []);
   return (
     <svg>
+      <Maelstrom x={200} y={400} size={100}></Maelstrom>
       {gameState.sheep.map((e) => (
-        <Dog key={e.id} x={e.x} y={e.y} heading={e.heading} color="green"></Dog>
+        <Sheep
+          key={e.id}
+          x={e.x}
+          y={e.y}
+          heading={e.heading}
+          color="green"
+        ></Sheep>
       ))}
       <Dog
         key={gameState.dog.id}
@@ -138,29 +195,53 @@ function App() {
             y={10}
             stroke="yellow"
             fill="yellow"
-            style={{ fontSize: "20px", dominantBaseline: 'hanging', textAnchor: 'start' }}
+            style={{
+              fontSize: "20px",
+              dominantBaseline: "hanging",
+              textAnchor: "start",
+            }}
           >
-            {gameState.calibration.top_left?.map(Math.round).join(',')}
+            {gameState.calibration.top_left?.map(Math.round).join(",")}
           </text>
-          <circle cx={0} cy={SCREEN_HEIGHT - 1} stroke="yellow" r={6} fill="yellow"></circle>
+          <circle
+            cx={0}
+            cy={SCREEN_HEIGHT - 1}
+            stroke="yellow"
+            r={6}
+            fill="yellow"
+          ></circle>
           <text
             x={10}
             y={SCREEN_HEIGHT - 1}
             stroke="yellow"
             fill="yellow"
-            style={{ fontSize: "20px", dominantBaseline: 'auto', textAnchor: 'start' }}
+            style={{
+              fontSize: "20px",
+              dominantBaseline: "auto",
+              textAnchor: "start",
+            }}
           >
-            {gameState.calibration.bottom_left?.map(Math.round).join(',')}
+            {gameState.calibration.bottom_left?.map(Math.round).join(",")}
           </text>
-          <circle cx={SCREEN_WIDTH - 1} cy={0} stroke="yellow" r={6} fill="yellow"></circle>
+          <circle
+            cx={SCREEN_WIDTH - 1}
+            cy={0}
+            stroke="yellow"
+            r={6}
+            fill="yellow"
+          ></circle>
           <text
             x={SCREEN_WIDTH - 1 - 10}
             y={10}
             stroke="yellow"
             fill="yellow"
-            style={{ fontSize: "20px", dominantBaseline: 'hanging', textAnchor: 'end' }}
+            style={{
+              fontSize: "20px",
+              dominantBaseline: "hanging",
+              textAnchor: "end",
+            }}
           >
-            {gameState.calibration.top_right?.map(Math.round).join(',')}
+            {gameState.calibration.top_right?.map(Math.round).join(",")}
           </text>
           <circle
             cx={SCREEN_WIDTH - 1}
@@ -174,9 +255,13 @@ function App() {
             y={SCREEN_HEIGHT - 1 - 10}
             stroke="yellow"
             fill="yellow"
-            style={{ fontSize: "20px", dominantBaseline: 'auto', textAnchor: 'end' }}
+            style={{
+              fontSize: "20px",
+              dominantBaseline: "auto",
+              textAnchor: "end",
+            }}
           >
-            {gameState.calibration.bottom_right?.map(Math.round).join(',')}
+            {gameState.calibration.bottom_right?.map(Math.round).join(",")}
           </text>
         </>
       )}
--- autokat/web/hooks/animation.ts
+++ autokat/web/hooks/animation.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const useAnimationFrame = (callback) => {
   // Use useRef for mutable variables that we want to persist
@@ -9,7 +9,7 @@ export const useAnimationFrame = (callback) => {
   const animate: FrameRequestCallback = (t) => {
     if (previousTimeRef.current != undefined) {
       const dt = t - previousTimeRef.current;
-      callback({t: t / 1000, dt: dt / 1000});
+      callback({ t: t / 1000, dt: dt / 1000 });
     }
     previousTimeRef.current = t;
     requestRef.current = requestAnimationFrame(animate);
@@ -19,4 +19,12 @@ export const useAnimationFrame = (callback) => {
     requestRef.current = requestAnimationFrame(animate);
     return () => cancelAnimationFrame(requestRef.current);
   }, []); // Make sure the effect runs only once
+};
+
+export const useAnimationTime = () => {
+  const [time, setTime] = useState(0);
+  useAnimationFrame(({ t }) => {
+    setTime(t);
+  });
+  return time;
 };
